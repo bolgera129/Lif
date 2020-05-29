1a06c22393da485f04f85dcb92f74d9294302055
Anna Bolger <bolgera@bc.edu>
2020-05-28 22:06:31 -0400

    product update tooltip

--- lif/src/Components/Main/Product.js
+++ lif/src/Components/Main/Product.js
@@ -1,12 +1,20 @@
-import React from "react"
+import React, { useState } from "react"
 import css from "../../css/Product.module.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faShoppingCart} from "@fortawesome/free-solid-svg-icons"
+import Button from "react-bootstrap/Button"
+import OverlayTrigger from "react-bootstrap/OverlayTrigger"
+import Tooltip from "react-bootstrap/Tooltip"
 
 
 export default function Product(props){
 
+    //const [cart, setCart] = useState();
+
     const shirt = props.shirt;
+
+    function addToCart () {
+    }
 
     return(
         <div>
@@ -14,7 +22,17 @@ export default function Product(props){
             <p>  
                 {shirt.description} $ {shirt.price}
                 <span className = "padding10">
-                  <FontAwesomeIcon icon = {faShoppingCart}/>
+                    <OverlayTrigger
+                      overlay ={ 
+                        <Tooltip>
+                            Add To Cart
+                        </Tooltip>
+                    }>
+                        <Button onClick = {addToCart} className = {css.shoppingCart}>
+                            <FontAwesomeIcon icon = {faShoppingCart}/>
+                        </Button>
+                    </OverlayTrigger>
+
                 </span>
               </p>
         </div>
